f156ec32d38433a2674eb2d501932b7a29260f22
Panciu-Rusu Mihai <panciu.rusu.mihai@gmail.com>
2018-11-30 23:24:13 +0200

    Csv input data conversion

--- Algorithm/FileOperations.py
+++ Algorithm/FileOperations.py
@@ -5,5 +5,5 @@ def importData(path):
     csv_file = open(path)
     csv_reader = csv.reader(csv_file, delimiter=',')
     for p in csv_reader:
-        input_pieces.append((p[0], p[1], p[2], p[3]))  # (x,y,z,culoare) tupla
+        input_pieces.append((int(p[0]), int(p[1]), int(p[2]), str(p[3])))  # (x,y,z,culoare) tupla
     return input_pieces
