0bf86012f93db632a16b7679de2f9a9677efd1ff
D-Andreea <das.andreea@gmail.com>
2018-12-11 20:42:39 +0200

    Method for breaking down layers
    it breaks down the layers from for the output pieces

--- Algorithm/center_of_mass.py
+++ Algorithm/center_of_mass.py
@@ -4,4 +4,16 @@
 def is_structure_stable(layers):
     for layer in layers:
         [print(piece) for piece in layer.merged_pieces]
-    
+    break_layer(layers)
+
+
+def break_layer(layers):
+    for layer in layers:
+        current_layer = -1
+        pieces_on_current_layer = []
+        for piece in layer.merged_pieces:
+            if current_layer == -1:
+                current_layer = piece.z
+            if piece.z == current_layer:
+                pieces_on_current_layer.append(piece)
+        
