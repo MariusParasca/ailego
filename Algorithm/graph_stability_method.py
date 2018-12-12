3ccdf0da3c9ec5ba5501c5b94ed8ac627ce02827
MariusParasca <mrs.parasca@gmail.com>
2018-12-12 12:50:18 +0200

    version 2 of graph stabilization method: more time efficent

--- Algorithm/graph_stability_method.py
+++ Algorithm/graph_stability_method.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from piece import Piece
+from layer import Layer
 
 
 def is_piece_combined(piece_parts, layer_):
@@ -11,17 +13,15 @@ def is_piece_combined(piece_parts, layer_):
     return None
 
 
-def create_nodes(G, layers_):
+def create_nodes_old(G, layers_):
     for i in range(0, len(layers_)):
         for piece, parts in layers_[i].items():
             G.add_node(piece)
-    # nx.draw(G)
-    # plt.show()
 
 
-def create_stability_graph(layers_):
+def create_stability_graph_old(layers_):
     G = nx.Graph()
-    create_nodes(G, layers_)
+    create_nodes_old(G, layers_)
     for i in range(0, len(layers_)-1):
         for piece, parts in layers_[i].items():
             result = is_piece_combined(parts, layers_[i + 1])
@@ -29,3 +29,69 @@ def create_stability_graph(layers_):
                 G.add_edge(piece, result)
     nx.draw(G)
     plt.show()
+
+
+def is_overlapping(x1, y1, x2, y2):
+    return max(x1, y1) >= min(x2, y2)
+
+
+def get_interval_margin(piece):
+    if piece.orientation:
+        piece_x = Piece.PIECES_TYPES[piece.piece_type][1] - 1 + piece.x
+        piece_y = Piece.PIECES_TYPES[piece.piece_type][0] - 1 + piece.y
+    else:
+        piece_x = Piece.PIECES_TYPES[piece.piece_type][0] - 1 + piece.x
+        piece_y = Piece.PIECES_TYPES[piece.piece_type][1] - 1 + piece.y
+
+    return piece_x, piece_y
+
+
+def create_nodes(G, layers_):
+    for i in range(0, len(layers_)):
+        for piece in layers_[i].merged_pieces:
+            G.add_node(piece)
+
+
+def create_stability_graph(layers_):
+    G = nx.Graph()
+    create_nodes(G, layers_)
+    for i in range(0, len(layers_) - 1):
+        for piece1 in layers_[i].merged_pieces:
+            for piece2 in layers_[i + 1].merged_pieces:
+                piece1_x, piece1_y = get_interval_margin(piece1)
+                piece2_x, piece2_y = get_interval_margin(piece2)
+                if is_overlapping(piece1.x, piece1_x, piece2.x, piece2_x) and \
+                   is_overlapping(piece1.z, piece1_y, piece2.z, piece2_y):
+                    G.add_edge(piece1, piece2)
+    nx.draw(G)
+    plt.show()
+    return G
+
+
+def read_output_from_file(file_path):
+    layers = []
+    piece_list = []
+    layer_number = 0
+    with open(file_path) as fd:
+        for line in fd.readlines():
+            if not line.strip():
+                continue
+            data = line.split(',')
+            if int(data[5]) == 0:
+                piece = Piece(int(data[0]), data[4], int(data[1]), int(data[2]), int(data[3]), False)
+            else:
+                piece = Piece(int(data[0]), data[4], int(data[1]), int(data[2]), int(data[3]), True)
+            if layer_number == int(data[2]):
+                piece_list.append(piece)
+            else:
+                layer = Layer([])
+                layer.merged_pieces = piece_list
+                layers.append(layer)
+                layer_number += 1
+                piece_list = [piece]
+    layer = Layer([])
+    layer.merged_pieces = piece_list
+    layers.append(layer)
+    return layers
+
+
--- Algorithm/main.py
+++ Algorithm/main.py
@@ -2,7 +2,7 @@ import os.path
 from file_operations import import_data, export_data
 from layer import Layer
 from model import Model
-from graph_stability_method import create_stability_graph
+from graph_stability_method import *
 
 def break_input_in_layers(input_pieces):
     max = 0
@@ -23,18 +23,22 @@ def merge_input_pieces(input_pieces):
     return model
 
 
+def get_detailed_layers(model):
+    array = []
+    for layer in model.layers:
+        dic = {}
+        for piece in layer.merged_pieces:
+            dic[(piece.x, piece.y, piece.z)] = piece.export()
+        array.append(dic)
+    return array
+
+
 if __name__ == '__main__':
     input_pieces = import_data(os.path.join(os.path.dirname(os.path.realpath(__file__)),'input.csv'))
     model = merge_input_pieces(input_pieces)
     export_data(os.path.join(os.path.dirname(os.path.realpath(__file__)), r'..\AI_Interface\input.csv'), model)
-    array = []
-    # Portiune de cod echipa cu testaterea stabilitati cu metoda grafurilor
-    # for layer in model.layers:
-    #     dic = {}
-    #     for piece in layer.merged_pieces:
-    #         dic[(piece.x, piece.y, piece.z)] = piece.export()
-    #     array.append(dic)
-    # print(dic)
-    # create_stability_graph(array)
+    # Portiune cod pentru cei de la metoda cu grafuri
+    # layers = read_output_from_file('E:\Dropbox\Facultate\Inteligenta artificiala\AI_Interface\input.csv')
+    # create_stability_graph(layers)
     for layer in model.layers:
         [print(piece) for piece in layer.merged_pieces]
